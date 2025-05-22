99bd79f62585c9360efc5b5ab45846e5ce3ab9b2
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-05-22 11:33:57 -0300

    recuperação de senha concluido

--- src/app/pages/auth/recover/recover.component.ts
+++ src/app/pages/auth/recover/recover.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { Auth } from '@angular/fire/auth';
-import { FormBuilder } from '@angular/forms';
+import { Auth, sendPasswordResetEmail } from '@angular/fire/auth';
+import { ToastController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recover',
   templateUrl: './recover.component.html',
   styleUrls: ['./recover.component.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, FormsModule,  CommonModule],
 })
 export class RecoverComponent  implements OnInit {
 
-  form = this.fb.group({
-    email: ['']
-  })
+  email: string = '';
 
   constructor(
     private auth: Auth,
     private router: Router,
-    private fb: FormBuilder
+    private toast: ToastController,
 
   ) { }
 
@@ -28,7 +28,36 @@ export class RecoverComponent  implements OnInit {
 
   }
 
-  recuperar() {}
+  async recuperar() {
+    if(!this.email) {
+      const toast = await this.toast.create({
+        message: 'Preencha o campo de email',
+        duration: 3000,
+        color: 'warning',
+      });
+      toast.present();
+      return;
+    }
+
+    try {
+      await sendPasswordResetEmail(this.auth, this.email);
+      const toast = await this.toast.create({
+        message: 'Email de recuperação enviado com sucesso',
+        duration: 3000,
+        color: 'success',
+      })
+      toast.present();
+      this.router.navigate(['/login']);
+    } catch(error) {
+      const toast = await this.toast.create({
+        message: 'Erro ao enviar email de recuperação, verifique o email e tente novamente',
+        duration: 3000,
+        color: 'danger',
+      })
+      toast.present();
+    }
+
+  }
 
   login() {
     this.router.navigate(['/login']);
